8698cb2850d2fd1a33bcdf6a91ef44e964d73b2c
ewellerstein <147428449+ewellerstein@users.noreply.github.com>
2023-12-13 11:13:21 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('revealButton').addEventListener('click', function() {
+    var topRightInfo = document.getElementById('top-right-info');
+    topRightInfo.classList.toggle('hidden');
+});
